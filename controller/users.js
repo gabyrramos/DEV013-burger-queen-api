06b568fc6b561922e44d755a3bec31bbf5c266c1
Gabriela Ramos <gabyr.contact@gmail.com>
2024-05-22 15:03:25 -0500

    cambios al CRUD

--- controller/users.js
+++ controller/users.js
@@ -1,16 +1,44 @@
 const users = require("../routes/users")
 bycrypt = require('bcrypt');
 const { connect } = require('../connect');
-const { Collection } = require("mongodb");
+const { Collection, ObjectId, MongoUnexpectedServerResponseError } = require("mongodb");
 const { adminEmail } = require("../config");
 
 
 
 module.exports = {
 
+  createAdminUser: async (adminUser) => {
+   try {
+    const db = await connect();
+    const usersCollection = db.connect('users');
+   
+    const foundAdmin = await usersCollection.findOne({ role: 'admin'});
+
+    if(!foundAdmin){
+      //aqui tengo que insertar tambien la pw? 
+      const hashedPassword = await bycrypt.hash(adminUser.password, 10);
+      const adminUserToInsert = {
+      ...adminUser,
+      role: 'admin',
+      password: hashedPassword,
+      }
+
+      const result = await usersCollection.insertOne(adminUserToInsert);
+      res.status(200).send('User creado') 
+  } else {
+    return res.status(400).send('Ya existe este usuario');
+  }
+  }
+  catch(error) {
+    console.log('Error creando el admin user');
+    return res.status(400).send('Ya existe el usuario');
+   }
+  },
+
   postUser: async (req, resp, next) => {
     try {
-      const { email, password, role } = req.body
+      const { email, password, role } = req.body;
       
 
       if (!email || !password) {
@@ -31,7 +59,7 @@ module.exports = {
         return resp.status(400).send('El email ya existe')      
       };
 
-      const creatingPassword = await bycrypt.hash(password, 10);
+      const creatingPassword = await bycrypt.hash(password, 10); //salt rounds 10
       const userCreated = await usersCollection.insertOne({
         email,
         password: creatingPassword,
@@ -44,7 +72,6 @@ module.exports = {
         email,
         role,
       });
-
     } catch (error) {
       console.error('Error:', error);
       return resp.status(500).send('Error, algo salio mal');
@@ -52,40 +79,56 @@ module.exports = {
   },
 
   getUser: async (req, resp, next) => {
+    try {  
       const db = await connect();
-      const page = req.query.page || 0;
+      const page = parseInt(req.query.page) || 0;
       const userPerPage = 10;
-      let users = []
-      db.collection('users')
+      const users = await db.collection('users')
       .find()
       .sort({ id: 1 })
       .skip(page * userPerPage)
       .limit(userPerPage)
-      .forEach(user => users.push(user))
-      .then(() =>{
-        resp.status(200).json(users)
-      })
-      .catch(() => {
+      .toArray()
+
+      resp.status(200).json(users);
+      } catch(error) {
+        console.log(error);
       resp.status(500).send('Algo salio mal');
-    })
+    }
   },
 
-  getUserAdmin: async (req, res, next) =>{
+  getUserById: async (req, res, next) =>{
     try{
       const db = await connect();
       const usersCollection = db.collection('users');
-      const users = await usersCollection.find();   
-       if (!adminEmail) {
-        return res.status()
+      const userID = req.params.id;
+      //aqui validamos el id
+      if(!ObjectId.isValid(userID)){
+        return res.status(400).send('ID no es valido');
+      }
+
+      const user = await usersCollection.findOne({ _id: new ObjectId(userID)});   
+
+       if (!user) {
+        return res.status(404).send('Error no hay usuario con ese ID')
+       } else {
+        return res.status(200).json(user);
        }
-      
-        }catch (error) {
-      return resp.status(500).send("Error, algo paso, intente de nuevo");
+   }catch (error) {
+      console.error('Error obteniendo el usuario por ID:', error);
+      return res.status(500).send('Error, algo paso, intente de nuevo');
     }
-  }
+  },
 
   updateUser: (req, resp, next) => {  //AQUI MANEJAMOS EL CRUD
     // TODO: Implement the necessary function to fetch the `users` collection or table
+      //acceder a la base datos
+      //acceder a la collection
+      // acceder a los parametros con los roles o IDS?
+      //digamos que quiero actualizar el email de una usuaria, 
+      //entonces accedo a ese correo
+      //y despues inserto el cambio 
+      //y lo guardo??
 
   },
 

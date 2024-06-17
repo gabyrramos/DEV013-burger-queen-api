c447407f459a71c5a8c550f0d5adec6bd8bd3e70
Gabriela Ramos <gabyr.contact@gmail.com>
2024-06-17 18:23:25 -0500

    Cambios a Vercel

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://127.0.0.1:27017/BurgerQueenAPI';
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb+srv://gabyrcontact:WFJYsqI1c2ezXoWU@cluster0.tyu8zpu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost.com';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
